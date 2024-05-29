577d2e8cf4b77e29d905cfc666600d7faef0abd4
sutharkamlesh <kamlesh@openeducat.com>
2024-05-29 15:28:54 +0530

    [REMOVE][17.0] remove entire hospital module

--- hospital_management/models/department.py
+++
@@ -1,7 +0,0 @@
-from odoo import models, fields
-
-class Department(models.Model):
-    _name = "hm.department"
-    _description = "Department Details"
-
-    name = fields.Char(string="Name", required=True)
--- hospital_management/models/dr_specialty.py
+++
@@ -1,9 +0,0 @@
-from odoo import models, fields
-
-class Specialties(models.Model):
-    _name = "dr.specialties"
-    _description = "Doctor Specialties"
-    
-    name = fields.Char(string="Name", required=True)
-    description = fields.Text(string="Description")
-    
